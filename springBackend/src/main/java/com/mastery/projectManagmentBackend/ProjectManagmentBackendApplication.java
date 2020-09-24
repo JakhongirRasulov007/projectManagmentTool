5267ac7e8840cb8db427b31e588ca7ef6685dbe8
Jakhongir <jakhongirrasulov001@gmail.com>
2020-09-24 21:51:25 +0500

    initial commit

---
+++ springBackend/src/main/java/com/mastery/projectManagmentBackend/ProjectManagmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mastery.projectManagmentBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagmentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagmentBackendApplication.class, args);
+	}
+
+}
---
+++ springBackend/src/test/java/com/mastery/projectManagmentBackend/ProjectManagmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastery.projectManagmentBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagmentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
